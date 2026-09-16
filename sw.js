9e48ba8f770cd57260cf2873d450b94c2e625754
Matheus Boettger <121169925+MatheusBoettger7@users.noreply.github.com>
2026-09-16 19:49:35 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'treino-trinca-v9';
+const CACHE = 'treino-trinca-v10';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon.svg'];
 
 self.addEventListener('install', event => {
